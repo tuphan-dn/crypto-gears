ac35dd070b6ac11612372212e927f8d2a06a3153
tuphan-dn <tuphan@descartes.network>
2022-12-24 21:14:05 +0700

    support ectss

---
+++ src/ectss.ts
@@ -0,0 +1,31 @@
+import { CURVE, Point } from '@noble/secp256k1'
+import BN from 'bn.js'
+import { RedBN } from './types'
+
+/**
+ * ECCurve
+ */
+export class ECCurve {
+  static red = BN.red(new BN(CURVE.P.toString()))
+
+  static encode = (r: Uint8Array): RedBN =>
+    new BN(r, 16, 'le').toRed(ECCurve.red)
+
+  static decode = (r: BN, length: number): Uint8Array =>
+    r.toArrayLike(Buffer, 'le', length)
+
+  static mod = (r: Uint8Array): Uint8Array =>
+    new BN(r, 16, 'le').toRed(ECCurve.red).toArrayLike(Buffer, 'le', r.length)
+
+  static baseMul = (r: Uint8Array): Uint8Array => {
+    const bn = new BN(r, 16, 'le')
+    const bi = BigInt(bn.toString())
+    return Point.BASE.multiply(bi).toRawBytes()
+  }
+
+  static addPoint = (aPubkey: Uint8Array, bPubkey: Uint8Array): Uint8Array => {
+    const a = Point.fromHex(aPubkey.subarray(0, 32))
+    const b = Point.fromHex(bPubkey.subarray(0, 32))
+    return a.add(b).toRawBytes()
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './sss'
+export * from './ectss'
 export * from './edtss'
 export * from './types'
